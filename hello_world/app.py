a473538c857bb4e9f090d32fea7d5fef3e1ac0ff
bocartmt <bocartmt@gmail.com>
2020-05-02 15:13:14 +0200

    gestion des codes d'erreur

--- hello_world/app.py
+++ hello_world/app.py
@@ -1,10 +1,12 @@
 import json
 from flask_lambda import FlaskLambda
 import boto3
+from botocore.errorfactory import ClientError
 import os
 from flask import request, redirect
 from uuid import uuid4
 from datetime import datetime
+from boto3.dynamodb.conditions import Key, Attr
 
 handler = FlaskLambda(__name__)
 s3 = boto3.client("s3")
@@ -39,17 +41,26 @@ def list_or_put_object():
             {'Content-Type':'application/json'}
         )
     elif request.method == "POST":
-        now = datetime.now()
-        dateAnTime = now.strftime("%d-%m-%Y %H:%M:%S")
         s3_key = request.get_json()["s3_key"]
-        short_key = str(uuid4())[:10]
-        short_key = short_key.replace("-", "")
-        response = tableName.put_item(Item = {"s3_key": s3_key, "short_key": short_key,"Creation_Date": dateAnTime})
-        return (
-            short_key,
-            200,
-            {'Content-Type':'application/json'}
-        )
+        try:
+            s3.head_object(Bucket= bucketName, Key= s3_key)
+        except ClientError as e:
+            if e.response['Error']['Code'] == "404":
+                return (
+                    {"message":"key not found"},
+                    404,
+                    {'Content-Type':'application/json'}
+                    )
+        else:   
+            now = datetime.now()
+            dateAnTime = now.strftime("%d-%m-%Y %H:%M:%S")
+            short_key = str(uuid4())[:10].replace("-", "")
+            response = tableName.put_item(Item = {"s3_key": s3_key, "short_key": short_key,"Creation_Date": dateAnTime})
+            return (
+                {"short_key":short_key},
+                200,
+                {'Content-Type':'application/json'}
+                )
     else:
         s3_key = request.get_json()["s3_key"]
         short_key = request.get_json()["short_key"]
@@ -60,19 +71,27 @@ def list_or_put_object():
             {'Content-Type':'application/json'}
             )
 
-@handler.route("/dowload", methods =["GET"])
+@handler.route("/download", methods =["GET"])
 def dowload_object():
+
     short_key = request.args.get("short_key")
     s3_key = request.args.get("s3_key")
-    item = tableName.get_item(Key = {"s3_key" : s3_key , "short_key": short_key })
-    s3_key = item["Item"]["s3_key"]
-
-    response = s3.generate_presigned_url('get_object',
+    record = tableName.query( KeyConditionExpression=Key('s3_key').eq(s3_key) & Key('short_key').eq(short_key))["Items"]
+    if len(record) != 0:
+        response = s3.generate_presigned_url('get_object',
                                                     Params={'Bucket': bucketName,
                                                             'Key': s3_key},
                                                     ExpiresIn=300)
-    return (
-        redirect(response),
-        302,
-        {'Content-Type':'application/json'}
-        )
+          
+        return  (redirect(response),
+                302,
+                {'Content-Type':'application/json'}
+                )
+    else:
+        return (
+            {"message":"404 not found"},
+            404,
+            {'Content-Type':'application/json'}
+            )
+
+    
